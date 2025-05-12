adf4e531089f0a567ce6af8ac6a4d9ed8d6f9c4e
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-05-12 11:35:40 +0200

    Affichage des images dans la galerie

---
+++ scripts/recuperationPhoto.js
@@ -0,0 +1,12 @@
+
+let divGalerie = document.getElementById('galerie');
+
+for(i =1; i < 7; i++){
+    let  img = document.createElement("img");
+    img.src = `gallery/${i}.jpg`;
+    img.alt = "image galerie";
+    img.className = "imageGalerie";
+
+    divGalerie.appendChild(img);
+}
+
