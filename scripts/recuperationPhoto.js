742c5154714efd40de519a2007de9a2b550100db
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-05-13 14:28:28 +0200

    Ajout de la possibilité de supprimer une photo ajouté dans la galerie

--- scripts/recuperationPhoto.js
+++ scripts/recuperationPhoto.js
@@ -4,6 +4,7 @@ let divGalerie = document.getElementById('galerie');
 let liste = JSON.parse(localStorage.getItem("listeUrl"));
 
 
+
 for (i = 1; i < nbImages; i++) {
     let img = document.createElement("img");
     img.src = `gallery/${i}.jpg`;
@@ -12,25 +13,34 @@ for (i = 1; i < nbImages; i++) {
 
     divGalerie.appendChild(img);
 }
+
+
 try {
     for (let i = 0; i < Object.keys(liste).length; i++) {
         let img = document.createElement("img");
         img.src = liste[i];
         img.alt = "image galerie";
-        img.className = "imageGalerie";
-        img.id = `${i}`;
+        img.className = "imageGalerie opacitySurvol";
+        img.id = i;
+        img.onclick = function () {
+            supElement(i);
+        }
 
         divGalerie.appendChild(img);
 
-        const image = document.getElementById(i);
-        const btn = document.createElement("button");
-        btn.value = "test";
-        btn.type = "button"
-        image.appendChild(btn);
     }
 } catch (error) {
 }
 function suppressionPhoto() {
     localStorage.clear();
     location.reload();
+}
+
+function supElement(id){
+    const element = document.getElementById(id);
+    liste = JSON.parse(localStorage.getItem("listeUrl"));
+
+    liste.splice(id, id + 1);
+    localStorage.setItem("listeUrl", JSON.stringify(liste));
+    element.remove();  
 }
